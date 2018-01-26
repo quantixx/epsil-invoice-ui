55c62aeae6806a32156d870ccfec53e1fc88c68a
Douglas Duteil <douglasduteil@gmail.com>
2018-01-26 16:38:28 +0100

    feat(playground): add a navbar and a home screen

--- playground/src/app/core/core.module.ts
+++ playground/src/app/core/core.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { PlaygroundAppRootComponent } from './playground-app-root/playground-app-root.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   imports: [CommonModule, RouterModule],
-  declarations: [PlaygroundAppRootComponent, PlaygroundAppRootComponent],
+  declarations: [PlaygroundAppRootComponent, PlaygroundAppRootComponent, NavbarComponent],
   exports: [PlaygroundAppRootComponent]
 })
 export class CoreModule {}
---
+++ playground/src/app/home/home-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { HomeComponent } from './home/home.component';
+
+//
+
+const routes: Routes = [{ path: '', component: HomeComponent }];
+
+@NgModule({
+  declarations: [HomeComponent],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class HomeRoutingModule {}
