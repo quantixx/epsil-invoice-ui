592e1d63af2268e862313e32adf24a2abc16ed6b
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-04-13 17:14:46 +0200

    table

---
+++ libs/invoices/src/editor/containers/invoice-table-view/invoice-table-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'epsil-invoice-table-view',
+  styleUrls: ['./invoice-table-view.component.scss'],
+  templateUrl: './invoice-table-view.component.html'
+})
+export class InvoiceTableViewComponent {}
--- libs/invoices/src/editor/editor.module.ts
+++ libs/invoices/src/editor/editor.module.ts
@@ -2,15 +2,16 @@
 
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { ClientViewComponent } from './containers/client-view/client-view.component';
 import { EditorLayoutComponent } from './components/editor-layout/editor-layout.component';
 import { EditorViewComponent } from './containers/editor-view/editor-view.component';
 import { TenantViewComponent } from './containers/tenant-view/tenant-view.component';
-import { ClientViewComponent } from './containers/client-view/client-view.component';
+import { InvoiceTableViewComponent } from './containers/invoice-table-view/invoice-table-view.component';
 
 //
 
 @NgModule({
-  declarations: [EditorViewComponent, EditorLayoutComponent, TenantViewComponent, ClientViewComponent],
+  declarations: [EditorViewComponent, EditorLayoutComponent, TenantViewComponent, ClientViewComponent, InvoiceTableViewComponent],
   imports: [CommonModule]
 })
 export class EditorModule {}
