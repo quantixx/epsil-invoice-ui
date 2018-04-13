72ae6eb2abb1535f53372541227820eea30da93d
Antonio Goncalves <antonio.goncalves@gmail.com>
2018-04-13 16:44:00 +0200

    component tenant view

---
+++ libs/invoices/src/editor/containers/tenant-view/tenant-view.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'epsil-tenant-view',
+  styleUrls: ['./tenant-view.component.scss'],
+  templateUrl: './tenant-view.component.html'
+})
+export class TenantViewComponent {}
--- libs/invoices/src/editor/editor.module.ts
+++ libs/invoices/src/editor/editor.module.ts
@@ -4,11 +4,12 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { EditorLayoutComponent } from './components/editor-layout/editor-layout.component';
 import { EditorViewComponent } from './containers/editor-view/editor-view.component';
+import { TenantViewComponent } from './containers/tenant-view/tenant-view.component';
 
 //
 
 @NgModule({
-  declarations: [EditorViewComponent, EditorLayoutComponent],
+  declarations: [EditorViewComponent, EditorLayoutComponent, TenantViewComponent],
   imports: [CommonModule]
 })
 export class EditorModule {}
